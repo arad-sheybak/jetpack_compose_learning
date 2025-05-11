3bfd89edeca4243e4277e019527052686649798d
Arad Sheybak <arad.sheybak@outlook.com>
2025-05-11 12:55:44 +0330

    simple text,simple annotated text and advanced(clickale part of text) annotated text added

---
+++ app/src/main/java/com/aradsheybak/jetpack_compose_learning/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.aradsheybak.jetpack_compose_learning
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.aradsheybak.jetpack_compose_learning.ui.theme.Jetpack_compose_learningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_compose_learningTheme {
+                Content()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Content() {
+//    SimpleText()
+//    SimpleAnnotatedString()
+    AdvancedAnnotatedString()
+}
+
+@Composable
+private fun SimpleText() {
+    Text(
+        modifier = Modifier
+            .size(300.dp)
+            .padding(10.dp)
+            .background(colorResource(R.color.teal_200)),
+        text = "Hello Jetpack Compose",
+        fontSize = 16.sp,
+        color = colorResource(R.color.purple_500),
+        fontWeight = FontWeight.ExtraBold,
+        fontStyle = FontStyle.Italic,
+        textAlign = TextAlign.Center,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Composable
+private fun SimpleAnnotatedString() {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = SpanStyle(colorResource(R.color.purple_200))) {
+                append("Annotated ")
+            }
+
+            withStyle(style = SpanStyle(fontWeight = FontWeight.ExtraBold)) {
+                append("Text")
+            }
+        }, textAlign = TextAlign.Center,
+        modifier = Modifier
+            .size(250.dp)
+            .padding(start = 0.dp, top = 100.dp, bottom = 0.dp, end = 0.dp)
+    )
+
+}
+
+@Composable
+private fun AdvancedAnnotatedString(context: Context = LocalContext.current) {
+    var textLayoutResult: TextLayoutResult? = null
+
+    val annotatedText = buildAnnotatedString {
+        append("for more info")
+        pushStringAnnotation(tag = "URL", annotation = "https://google.com")
+        withStyle(
+            style = SpanStyle(
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.ExtraBold
+            )
+        ) {
+            append(" click here")
+        }
+
+        pop()
+    }
+    var clickedUrl by remember { mutableStateOf("") }
+
+    Text(
+        modifier = Modifier
+            .size(300.dp)
+            .background(colorResource(R.color.teal_200))
+            .padding(start = 0.dp, top = 100.dp, bottom = 0.dp, end = 0.dp).clickable{
+            }.pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    val layoutResult = textLayoutResult ?: return@detectTapGestures
+                    val position = layoutResult.getOffsetForPosition(offset)
+                    annotatedText.getStringAnnotations("URL", position, position)
+                        .firstOrNull()?.let { annotation ->
+                            clickedUrl = annotation.item
+                            Toast.makeText(context, "${annotation.tag} : ${annotation.item}", Toast.LENGTH_SHORT).show()
+                        }
+
+                }
+            },
+        onTextLayout = { result ->
+            textLayoutResult = result
+        },
+        text = annotatedText,
+        color = colorResource(R.color.purple_500),
+        textAlign = TextAlign.Center,
+    )
+
+}
+
+
+@Composable
+@Preview(showBackground = true)
+private fun preview() {
+//    SimpleText()
+//    SimpleAnnotatedString()
+    AdvancedAnnotatedString()
+}
